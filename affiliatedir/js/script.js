2b3737198d119ee0f7430d1dadfb15d97a68a1de
Roman <roman.sav15@gmail.com>
2021-04-08 18:05:53 +0300

    home page

--- affiliatedir/js/script.js
+++ affiliatedir/js/script.js
@@ -87,6 +87,20 @@ let front = {
           }
         })
       }
+      $(document).ready(function() {
+        $(".accordion__item .accordion__button").on("click", function(e) {
+        e.preventDefault();
+            if ($(this).parent().hasClass("active")) {
+            $(this).parent().removeClass("active");
+            $(this).parent().find(".accordion__content").slideUp(200);
+            } else {
+            $(".accordion__item").removeClass("active");
+            $(this).parent().addClass("active");
+            $(".accordion__content").slideUp(200);
+            $(this).parent().find(".accordion__content").slideDown(200);
+            }
+        });
+      });
   }
 };
 
